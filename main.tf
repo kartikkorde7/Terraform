7dce76e978629c666e18773589fdac7bfefec2f8
kartikkorde7 <kartik.korde07@gmail.com>
2026-09-01 13:30:50 +0530

    Fix tags syntax for AWS EC2 instance in Terraform configuration

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_ec2"{
     ami= "ami-090d68841c2a28756"
     instance_type= "t3.micro"
     key_name = "KARTIK"
-    tags {
+    tags =  {
             Name = "ec2"
     }
 }
